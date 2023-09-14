c8268cf060ca3c32957aa60acd4fee726e5bb9cd
Keith Lohnes <krlohnes@users.noreply.github.com>
2023-09-13 20:37:11 -0400

    Update sync script

--- sync.sh
+++ sync.sh
@@ -4,3 +4,4 @@ cp -R ~/Dropbox/Apps/Obsidian/Flame\ Campaign docs
 rm docs/Flame\ Campaign/Items.md
 rm -rf docs/Flame\ Campaign/Sessions
 rm docs/Flame\ Campaign/Annie\'s\ other\ notes.md
+rm docs/Flame\ Campaign/Feedback\ on\ DNDB.md
